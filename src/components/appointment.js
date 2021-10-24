b07e55643a86343180e4072d9fdfbe1bcfc3d193
TanyaRamesh3199 <tanyaramesh030199@gmail.com>
2021-10-24 17:47:28 +0800

    Added validation and live changes

--- src/components/appointment.js
+++ src/components/appointment.js
@@ -98,30 +98,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-function schedulerData(record){
-    const detail = record.name.toString() + record.order.toString();
+function schedData(record){
+    const detail = record.name.toString() + ' ' + record.order.toString();
     const timings = record.time.toString().split("-");
-    const startTime = record.date.toString() + 'T' + timings[0]
-    const endTime = record.date.toString() + 'T' + timings[1]
-    const schedule = {startDate: {startTime} , endDate: {endTime} , title : {detail}}
+    const new_date = new Date(record.date).toISOString().split('T')[0];
+    const startTime = new_date + 'T' + timings[0];
+    const endTime = new_date + 'T' + timings[1];
+    const schedule = {startDate: startTime , endDate: endTime , title : detail}
     return schedule
 }
 
-function createCalender(bookings_list){
-    const calender = [];
-    bookings_list.forEach(booking => calender.push(schedulerData(booking)));
-    return calender;
-}
+
 
 const currentDate = '2018-11-18';
 
-/*const [week, setWeek] = React.useState([
-        { name: 'Tanya', order: 'Bodycon Dress', date: '2018-11-20', time: '15:00-16:30' },
-        { name: 'Taehyung', order: 'Palazzo Embroidery',date: '2018-11-23' , time: '15:00-17:30'},
-        { name: 'Yoongi', order: 'Altered Pants', date :'2018-11-21', time: '12:00-14:30' },
-        { name: 'Heoseok', order: 'Sequin Blazer', date: '2018-11-24', time: '11:00-12:30'},
-        { name: 'Saniya', order: 'Skirt Dyeing', date: '2018-11-19', time: '16:00-17:30' }
-    ]);*/
 
 export default function Appointment(){
 
@@ -129,30 +119,25 @@ export default function Appointment(){
 
     const { useState } = React;
 
-    const schedulerData = [
-        { startDate: '2018-11-20T15:00', endDate: '2018-11-20T16:30', title: 'Tanya Bodycon Dress' },
-        { startDate: '2018-11-21T12:00', endDate: '2018-11-21T14:30', title: 'Yoongi Altered Pants' },
-        { startDate: '2018-11-23T15:00', endDate: '2018-11-23T17:30', title: 'Natasha Palazzo Embroidery' },
-        { startDate: '2018-11-24T11:00', endDate: '2018-11-24T12:30', title: 'Hema Sequin Blazer' },
-        { startDate: '2018-11-19T16:00', endDate: '2018-11-19T17:30', title: 'Saniya Skirt Dyeing' }
-    ];
-
     const [columns, setColumns] = useState([
-        { title: 'Name', field: 'name'},
-        { title: 'Order Description', field: 'order' },
-        { title: 'Date', field: 'date'},
-        { title: 'Time', field: 'time'}
+        { title: 'Name', field: 'name', type : 'string',validate: rowData => rowData.name === '' ? 'Name cannot be empty' : ''},
+        { title: 'Order Description', field: 'order' , validate: rowData => rowData.order === '' ? 'Order cannot be empty' : ''},
+        { title: 'Date', field: 'date', type: 'date', dateSetting: {
+                format: 'dd/MM/yyyy'},  validate: rowData => rowData.date === '' ? 'Date cannot be empty' : ''},
+        { title: 'Time', field: 'time' ,validate: rowData => rowData.time === '' ? 'Time cannot be empty' : ''}
 
     ]);
 
     const appt_info = []
+    const calender = []
 
     useEffect(() => {
         const fetchData = async () => {
             const details = await axios(
                 'http://localhost:3001/demoApi/appts',
             );
-            details.data.forEach(x => appt_info.push({id : x.apptId, name: x.orderCustomer, order: x.orderDescription , date:  x.apptDate , time : x.apptTime}))
+            details.data.forEach(x => appt_info.push({id : x.apptId, name: x.orderCustomer, order: x.orderDescription , date:  x.apptDate , time : x.apptTime}));
+            details.data.forEach(x => calender.push(schedData({name: x.orderCustomer, order: x.orderDescription , date:  x.apptDate , time : x.apptTime})));
         };
 
         fetchData();
@@ -160,7 +145,11 @@ export default function Appointment(){
 
 
     const [data, setData] = React.useState(appt_info);
-    //const schedulerData= createCalender(appt_info);
+    const [calData , setCal] = React.useState(calender);
+    const ApptList = calData;
+
+
+
 
 
     const handleChange = (event, newValue) => {
@@ -169,6 +158,7 @@ export default function Appointment(){
 
     // Add new rows : POST method
     const handleRowAdd = (newData) => {
+
         const postAppt = {
             'orderCustomer' : newData.name,
             'orderDescription': newData.order,
@@ -188,6 +178,7 @@ export default function Appointment(){
     // Edit the rows :  PUT method
 
     const handleRowUpdate = (updateData) => {
+
         const putAppt = {
             'orderCustomer' : updateData.name,
             'orderDescription': updateData.order,
@@ -283,7 +274,7 @@ export default function Appointment(){
                         <div style={{width: '100%' }}>
 
                                     <Scheduler
-                                        data = {schedulerData}
+                                        data = {ApptList}
                                     >
                                         <ViewState
                                             currentDate={currentDate}
@@ -294,7 +285,6 @@ export default function Appointment(){
                                         />
                                         <Appointments/>
                                     </Scheduler>
-
                         </div>
             </TabPanel>
         </div>
